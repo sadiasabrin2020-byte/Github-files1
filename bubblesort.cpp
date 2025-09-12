e77a1aa58c130cf7de66c890168b63c864295427
sadiasabrin2020-byte <sadia.sabrin2020@gmail.com>
2025-09-12 23:58:34 +0600

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"Enter the elements number:"<<endl;
+  cin>>n;
+  cout<<"Enter the elements :  "<<endl;
+  int arr[n];
+  for(int i=0;i<n;i++)
+   {
+       cin>>arr[i];
+   }
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
