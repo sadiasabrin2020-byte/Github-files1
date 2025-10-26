2ee485ad1eeab9cd60d1e6a6c0b2f32374aa0719
sadiasabrin2020-byte <sadia.sabrin2020@gmail.com>
2025-10-27 00:58:17 +0600

    Add files via upload

---
+++ Codeforce 3D.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; ++i) {
+        if (A[i] <= 10) {
+            cout << "A[" << i << "] = " << A[i] << endl;
+        }
+    }
+
+    return 0;
+}
