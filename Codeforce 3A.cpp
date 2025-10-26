5f7d4d9e9eca5208873af42e339a13442b48279e
sadiasabrin2020-byte <sadia.sabrin2020@gmail.com>
2025-10-27 00:20:38 +0600

    Add files via upload

---
+++ Codeforce 3A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    long long arr[n];
+    long long sum=0;
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+    sum+=arr[i];
+}
+cout<<abs(sum)<<endl;
+return 0;
+}
