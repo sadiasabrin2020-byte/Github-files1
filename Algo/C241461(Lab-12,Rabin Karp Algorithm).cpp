fe3fde3e65dc422d25868f6b2beb7cd425b9a799
sadiasabrin2020-byte <sadia.sabrin2020@gmail.com>
2025-11-09 23:25:18 +0600

    Add files via upload

---
+++ Algo/C241461(Lab-12,Rabin Karp Algorithm).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void rabinKarp(string text, string pattern)
+{
+    int n = text.size();
+    int m = pattern.size();
+    int found = 0;
+
+    for (int i = 0; i <= n - m; i++)
+    {
+        string sub = text.substr(i, m);
+        if (sub == pattern)
+        {
+            cout << "Pattern found at index " << i << endl;
+            found = 1;
+        }
+    }
+
+    if (!found)
+        cout << "Pattern not found." << endl;
+}
+
+int main()
+{
+    string text = "aabcabcab";
+    string pattern = "abc";
+    rabinKarp(text, pattern);
+    return 0;
+}
